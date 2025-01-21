04cc5a66126ebf9b1397fc57898a85efda1e1b75
Dks2006-crypto <denis18kpina@gmail.com>
2025-01-21 18:21:36 +0300

    Сделали много чего

--- dlog/urls.py
+++ dlog/urls.py
@@ -4,5 +4,6 @@ from .import views
 app_name = 'dlog'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('blog/<slug:slug>/', views.details, name='details')
 ]
--- dlog/views.py
+++ dlog/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return render(request, 'dlog/index.html')
+    posts = Post.objects.filter(is_active=True, is_banner=True).order_by('-created_at')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'dlog/index.html', context)
+
+def details(request, slug):
+    post = get_object_or_404(Post, slug=slug, is_active=True)
+    context = {
+        'post': post,
+        'title': post.title,
+    }
+    return render(request, 'dlog/details.html', context)
